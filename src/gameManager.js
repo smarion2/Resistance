b16a1eec40f046bec9fe122c0b261ba61a034c78
Sam Marion <smarion2@gmail.com>
2019-03-06 21:41:40 -0500

    sending spies all of their fellow buddies and doing a lazy reshuffle so its not immediatly obvious who the spies are. At least break it out into a function man!

--- src/gameManager.js
+++ src/gameManager.js
@@ -20,9 +20,25 @@ exports.startGame = function (sessionId) {
     if (session) {
         assignPlayerRoles(session.players);
         var players = getPlayerNames(session.players);
+        var otherSpies = [];
         for (var player in session.players) {
-            console.log('Roles have been assigned, time to start the game');            
-            session.players[player].connection.sendUTF(JSON.stringify({ messageType: 'startGame', role: session.players[player].role, players: players }));
+            if (session.players[player].role === 'red') {
+                otherSpies.push(session.players[player].name);
+            } 
+        }
+        console.log('otherSpies: ' + JSON.stringify(otherSpies));
+        for (var player in session.players) {
+            console.log('Roles have been assigned, time to start the game');
+            var message = { 
+                messageType: 'startGame',
+                role: session.players[player].role,
+                players: players                
+            };
+            if (session.players[player].role === 'red') {
+                message.otherSpies = otherSpies;
+            }
+            console.log('MESSAGE: ' + JSON.stringify(message));
+            session.players[player].connection.sendUTF(JSON.stringify(message));
         }
     }
 };
@@ -216,6 +232,11 @@ function assignPlayerRoles(players) {
         players[playerIndex].role = 'red';
         console.log('Player ' + players[playerIndex].name + ' is red');
         playerIndex++;
+    }
+    // lazy for now
+    for (let i = players.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [players[i], players[j]] = [players[j], players[i]];
     }
 }
 
