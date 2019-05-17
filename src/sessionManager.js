ff1e2cb8557b6362d976134b4bce990b0ea7f75f
Sam Marion <smarion2@gmail.com>
2019-05-16 20:09:49 -0400

    actually creating a game id. Kinda of wanted to reuse the otehr function but i dunno

--- src/sessionManager.js
+++ src/sessionManager.js
@@ -55,7 +55,13 @@ exports.resetMissionLeader = function (sessionId) {
 }
 
 function createGameId() {
-    return 'aaaa';
+    var result = '';
+    var characters  = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < 4; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
 }
 
 function createSession(server) {
