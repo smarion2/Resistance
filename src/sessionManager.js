37844f6d03be3d343583b803cb8b1460985b5b18
Sam Marion <smarion2@gmail.com>
2019-05-19 20:38:03 -0400

    Made more possible combinations for game id

--- src/sessionManager.js
+++ src/sessionManager.js
@@ -56,7 +56,7 @@ exports.resetMissionLeader = function (sessionId) {
 
 function createGameId() {
     var result = '';
-    var characters  = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var characters  = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
     var charactersLength = characters.length;
     for ( var i = 0; i < 4; i++ ) {
        result += characters.charAt(Math.floor(Math.random() * charactersLength));
